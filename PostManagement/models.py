cc13dce21c40d012640881a6ebc8222a2b3ea85a
rafsan <rafsanoff@gmail.com>
2022-08-12 23:47:25 +0600

    Bootstrap

--- PostManagement/models.py
+++ PostManagement/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -18,6 +19,7 @@ class Post(models.Model):
     #Post_image3 = models.ImageField(null='true',blank='true')
     #Post_description3 = models.TextField(max_length=100000, null='true',blank='true')
 
+    User = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.Post_title
