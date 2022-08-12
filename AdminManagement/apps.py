8766a82751abf6d6713acf1aa76598a972802c9e
rafsan <rafsanoff@gmail.com>
2022-08-13 00:07:37 +0600

    Travelxp Remove

--- AdminManagement/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminmanagementConfig(AppConfig):
-    name = 'AdminManagement'
--- PostManagement/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostmanagementConfig(AppConfig):
-    name = 'PostManagement'
